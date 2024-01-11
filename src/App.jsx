9aba60dc76eee072da12b899f35e6e254383c409
Barbara Viana <vianabsouza@gmail.com>
2024-01-11 17:36:58 -0300

    create 'post' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 import "./global.css";
@@ -10,7 +11,10 @@ export default function App() {
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-        <main>Post</main>
+        <main>
+          <Post />
+          <Post />
+        </main>
       </div>
     </div>
   );
---
+++ src/components/Post.jsx
@@ -0,0 +1,36 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/vianabsouza.png"
+            alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>Barbara Viana</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="1 de Janeiro às 08:13h" dateTime="2024-01-01 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>👉 jane.design/doctorcare</p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
