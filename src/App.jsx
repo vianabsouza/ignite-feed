649433612ea5bcb98bba4facc730f67f6f13b82c
Barbara Viana <vianabsouza@gmail.com>
2024-01-19 18:22:39 -0300

    change the values of idetifier key

--- src/App.jsx
+++ src/App.jsx
@@ -14,33 +14,31 @@ const posts = [
       role: "Web developer",
     },
     content: [
-      { id: 2, type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Fala galeraa 👋" },
       {
-        id: 3,
         type: "paragraph",
         content:
           "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
       },
-      { id: 4, type: "link", content: "👉 jane.design/doctorcare" },
+      { type: "link", content: "👉 jane.design/doctorcare" },
     ],
     publishedAt: new Date("2024-01-15 10:00:00"),
   },
   {
-    id: 5,
+    id: 2,
     author: {
       avatarUrl: "https://github.com/maykbrito.png",
       name: "Mayk Brito",
       role: "Educator",
     },
     content: [
-      { id: 5, type: "paragraph", content: "Fala galeraa 👋" },
+      { type: "paragraph", content: "Fala galeraa 👋" },
       {
-        id: 6,
         type: "paragraph",
         content:
           "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
       },
-      { id: 7, type: "link", content: "👉 jane.design/doctorcare" },
+      { type: "link", content: "👉 jane.design/doctorcare" },
     ],
     publishedAt: new Date("2024-01-17 10:00:00"),
   },
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -54,10 +54,10 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type == "paragraph") {
-            return <p key={line.id}>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type == "link") {
             return (
-              <p key={line.id}>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -77,8 +77,8 @@ export function Post({ author, content, publishedAt }) {
         </footer>
       </form>
       <div className={styles.commentList}>
-        {comments.map((comment, index) => {
-          return <Comment key={index} content={comment} />;
+        {comments.map((comment) => {
+          return <Comment key={comment} content={comment} />;
         })}
       </div>
     </article>
